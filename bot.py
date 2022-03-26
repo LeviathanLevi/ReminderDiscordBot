7337c9cbac521860c2221dfa11a084e5e2badd03
LeviathanLevi <leutholdlevi@gmail.com>
2022-03-25 18:20:48 -0700

    Bot now retrieves the specified user
    Bot now can retrieve a target users information and call methods on the user such as user.send("message")

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+import os
+import asyncio
 import discord
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-client = discord.Client()
-
+client = commands.Bot(command_prefix='', self_bot=True)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+TARGET_USER_ID = os.getenv('USER_TARGET_ID')
+
+
 
 @client.event
 async def on_ready():
@@ -12,10 +17,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+async def getUser(TARGET_USER_ID):
+    await client.wait_until_ready()
+    user = None
+    if user == None:
+        user = await client.fetch_user(TARGET_USER_ID)
+        print('Target User:',user)
+
+    return
+
+
+client.loop.create_task(getUser(TARGET_USER_ID))
+
 client.run(TOKEN)
+
+#await user.send("Hello")
+#print('message sent')
